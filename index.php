81c7d11a52cc279f5d5ab1caa9b0ee6ce1546399
parasjuneja641 <78646001+parasjuneja641@users.noreply.github.com>
2021-02-06 19:59:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
-<?php
-echo "hello";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Signup</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Login Here</h2>
+                <form action="signup.php" method="post"></form>
+                 <div class="form-group">
+                     <label>username</label>
+                     <input type="text" name="user" class="form-control">
+                 </div>
+                   
+                       <div class="form-group">
+                         <label>password</label>
+                         <input type="password" name="password" class="form-control">
+                       </div>
+                       <button class="btn btn-primary">Login</button>
+                                 
+            </div>
+            <div class="col-md-6">
+                <h2>Signin form</h2>
+                <form action="signup.php" method="post"></form>
+                 <div class="form-group">
+                     <label>username</label>
+                     <input type="text" name="user" class="form-control">
+                 </div>
+                   
+                       <div class="form-group">
+                         <label>password</label>
+                         <input type="password" name="password" class="form-control">
+                       </div>
+                       <button class="btn btn-primary">Login</button>
+                                 
+            </div>
+        </div>
+    </div>
+</body>
+</html>
